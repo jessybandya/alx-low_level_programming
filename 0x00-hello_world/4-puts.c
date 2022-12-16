1eef8445ca88717269f1daeae355c188f2cd625b
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-12-17 00:30:59 +0300

    Done task 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+<<<<<<< HEAD
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+=======
+/**
+ * main E - Entry Point
+ *
+ * Return always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like buildong a multilingual puzzle");
+>>>>>>> 7ca42a3c8e79508548f9461b663187ef30150c85
+	return (0);
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Main Directory README content
