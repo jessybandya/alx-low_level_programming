2e015717fb5a9354d7302be682552985c9276ce2
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-12-24 16:03:19 +0300

    redone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
-
 /**
- * main - Entry Point
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-=======
-/**
- * main E - Entry Point
- *
- * Return always 0 (success)
- */
-int main(void)
-{
-	puts("\"Programming is like buildong a multilingual puzzle");
->>>>>>> 7ca42a3c8e79508548f9461b663187ef30150c85
 	return (0);
-
 }
